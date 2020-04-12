de34ea3b4132217c32e5b58ee14e9fc059df4abf
Giorgio Grassini <giorgiograssini@gmail.com>
2020-04-12 19:55:53 +0200

    Hotifix(blog-post): fixed typo

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,7 +10,7 @@ class BlogPostTemplate extends React.Component {
   render() {    
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContex
+    const { previous, next } = this.props.pageContext
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
