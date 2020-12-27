7a6200b632a29e73eeeab5e4a0c13886f50e3db6
Giorgio Grassini <giorgiograssini@gmail.com>
2020-12-27 14:35:56 +0100

    tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "257201423",
+        trackingId: "G-DBB1TWSFM3",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
